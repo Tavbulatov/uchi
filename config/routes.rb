17d053693cf966aad0af0e2d00e173f0711f0b87
Tavbulatov <alikhantam26@gmail.com>
2024-03-07 23:41:27 +0300

    routes determination

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
   devise_for :students, controllers: { sessions: 'students/sessions',
                                        registrations: 'students/registrations' }
 
+  resources :schools, only: %i[] do
+    resources :classrooms, only: :index do
+      resources :students, only: %i[index]
+    end
+  end
 end
